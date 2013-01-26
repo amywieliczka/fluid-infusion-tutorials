159511fa8c2c5e65640f10d9b0d032a9065b191c
amy wieliczka <amywieliczka@berkeley.edu>
2013-01-25 17:14:46 -0800

    Fixing whitespace issues with textmate

--- georefTrial/georeference.js
+++ georefTrial/georeference.js
@@ -18,96 +18,96 @@ var cspace = cspace || {};
     fluid.defaults("cspace.georeference", {
         gradeNames: ["fluid.rendererComponent", "autoInit"],
         selectors: {
-			georefBtn: ".csc-georefBtn",
+            georefBtn: ".csc-georefBtn",
             georefSrc: ".csc-georeferenceSrc",
-			
-			georefLatitude: ".csc-georefLatitude",
-			georefLongitude: ".csc-georefLongitude",
-			georefDatum: ".csc-georefDatum",
-			georefUncertainty: ".csc-georefUncertainty",
-			georefUncertaintyUnits: ".csc-georefUncertaintyUnits",
-			georefProtocol: ".csc-georefProtocol",
+            
+            georefLatitude: ".csc-georefLatitude",
+            georefLongitude: ".csc-georefLongitude",
+            georefDatum: ".csc-georefDatum",
+            georefUncertainty: ".csc-georefUncertainty",
+            georefUncertaintyUnits: ".csc-georefUncertaintyUnits",
+            georefProtocol: ".csc-georefProtocol",
             georefRemarks: ".csc-georefRemarks"
-		},
-		model: {
-			source: "Troy",
-			latitude: "",
-			longitude: "",
-			datum: "",
-			uncertainty: "",
-			uncertaintyUnits: "",
-			protocol: "",
-			remarks: ""
-		},
-		events: {
-			onGeoRef: null,
-		},
-		produceTree: "cspace.georeference.produceTree",
-		finalInitFunction: "cspace.georeference.finalInit",
-		renderOnInit: true
-	});
-	
-	cspace.georeference.produceTree = function (that) {
-		return {
-			georefSrc: "${source}",
-			georefLatitude: "${latitude}",
-			georefLongitude: "${longitude}",
-			georefDatum: {
-			    optionnames: ["Please select a value", "Not Recorded", "ADG66", "NAD27", "NAD83", "NAD83&WGS84", "WGS84"],
-			    optionlist: ["", "Not Recorded", "ADG66", "NAD27", "NAD83", "NAD83&WGS84", "WGS84"],
-			    selection: "${datum}"
-			},
-			georefUncertainty: "${uncertainty}",
-			georefUncertaintyUnits: {
-			    optionnames: ["Please select a value", "unknown", "feet", "kilometers", "meters", "miles"],
-			    optionlist: ["", "unknown", "feet", "kilometers", "meters", "miles"],
-			    selection: "${uncertaintyUnits}"
-			},
-			georefProtocol: {
-			    optionnames: ["Please select a value", "Chapman, Wieczorek 2006, Guide to Best Practices for Georeferencing", "MaNIS/HerpNet/ORNIS Georeferencing Guidelines", "Georeferencing For Dummies", "BioGeomancer", "Google Maps GeoCoding Service API v3"],
-			    optionlist: ["", "chapman-wieczorek-2006-guide-best-practices-georeferencing", "manis-herpnet-ornis-georeferencing-guidelines", "georeferencing-dummies", "biogeomancer", "Google Maps GeoCoding Service API v3"],
-			    selection: "${protocol}"
-			},
-			georefRemarks: "${remarks}",
-			georefBtn: {
-				decorators: [{
-					type: "jQuery",
-					func: "click",
-					args: (
-					    function() { 
-					        //calls georefjs - passing an anonymous callback func that takes an array of georef objects
-					        georefjs.googleGeoref(that.model.source, function(georefs) { 
-					            //anonymous func basically a wrapper for displayMap function in order to add additional 
-					            //source context and function context (populateTargetFields) to the array of georef objects
-					            //displayMap displays a map for the user to choose one georef object from, then calls
-					            //populateTargetFields with the selected georef object as the parameter
-					            displayMap(that.model.source, georefs, function (result) {
-                                        that.applier.requestChange("latitude", result.decimalLatitude);
-                                        that.applier.requestChange("longitude", result.decimalLongitude);
-                                        that.applier.requestChange("datum", result.geodeticDatum);
-                                        that.applier.requestChange("uncertainty", result.coordinateUncertaintyInMeters);
-                                        that.applier.requestChange("uncertaintyUnits", "meters");
-                                        that.applier.requestChange("protocol", result.georeferenceProtocol);                             
-                                        that.applier.requestChange("remarks", result.georeferenceRemarks);
-                                        that.refreshView();
-                                        $(that.options.selectors.georefLatitude).css({"background": "#fffbbb"});
-                                        $(that.options.selectors.georefLongitude).css({"background": "#fffbbb"});
-                                        $(that.options.selectors.georefDatum).css({"background": "#fffbbb"});                                
-                                        $(that.options.selectors.georefUncertainty).css({"background": "#fffbbb"});
-                                        $(that.options.selectors.georefUncertaintyUnits).css({"background": "#fffbbb"});
-                                        $(that.options.selectors.georefProtocol).css({"background": "#fffbbb"});
-                                        $(that.options.selectors.georefRemarks).css({"background": "#fffbbb"});	    
+        },
+        model: {
+            source: "Troy",
+            latitude: "",
+            longitude: "",
+            datum: "",
+            uncertainty: "",
+            uncertaintyUnits: "",
+            protocol: "",
+            remarks: ""
+        },
+        events: {
+            onGeoRef: null,
+        },
+        produceTree: "cspace.georeference.produceTree",
+        finalInitFunction: "cspace.georeference.finalInit",
+        renderOnInit: true
+    });
+    
+    cspace.georeference.produceTree = function (that) {
+        return {
+            georefSrc: "${source}",
+            georefLatitude: "${latitude}",
+            georefLongitude: "${longitude}",
+            georefDatum: {
+                optionnames: ["Please select a value", "Not Recorded", "ADG66", "NAD27", "NAD83", "NAD83&WGS84", "WGS84"],
+                optionlist: ["", "Not Recorded", "ADG66", "NAD27", "NAD83", "NAD83&WGS84", "WGS84"],
+                selection: "${datum}"
+            },
+            georefUncertainty: "${uncertainty}",
+            georefUncertaintyUnits: {
+                optionnames: ["Please select a value", "unknown", "feet", "kilometers", "meters", "miles"],
+                optionlist: ["", "unknown", "feet", "kilometers", "meters", "miles"],
+                selection: "${uncertaintyUnits}"
+            },
+            georefProtocol: {
+                optionnames: ["Please select a value", "Chapman, Wieczorek 2006, Guide to Best Practices for Georeferencing", "MaNIS/HerpNet/ORNIS Georeferencing Guidelines", "Georeferencing For Dummies", "BioGeomancer", "Google Maps GeoCoding Service API v3"],
+                optionlist: ["", "chapman-wieczorek-2006-guide-best-practices-georeferencing", "manis-herpnet-ornis-georeferencing-guidelines", "georeferencing-dummies", "biogeomancer", "Google Maps GeoCoding Service API v3"],
+                selection: "${protocol}"
+            },
+            georefRemarks: "${remarks}",
+            georefBtn: {
+                decorators: [{
+                    type: "jQuery",
+                    func: "click",
+                    args: (
+                        function() { 
+                            //calls georefjs - passing an anonymous callback func that takes an array of georef objects
+                            georefjs.googleGeoref(that.model.source, function(georefs) { 
+                                //anonymous func basically a wrapper for displayMap function in order to add additional 
+                                //source context and function context (populateTargetFields) to the array of georef objects
+                                //displayMap displays a map for the user to choose one georef object from, then calls
+                                //populateTargetFields with the selected georef object as the parameter
+                                displayMap(that.model.source, georefs, function (result) {
+                                    that.applier.requestChange("latitude", result.decimalLatitude);
+                                    that.applier.requestChange("longitude", result.decimalLongitude);
+                                    that.applier.requestChange("datum", result.geodeticDatum);
+                                    that.applier.requestChange("uncertainty", result.coordinateUncertaintyInMeters);
+                                    that.applier.requestChange("uncertaintyUnits", "meters");
+                                    that.applier.requestChange("protocol", result.georeferenceProtocol);                             
+                                    that.applier.requestChange("remarks", result.georeferenceRemarks);
+                                    that.refreshView();
+                                    $(that.options.selectors.georefLatitude).css({"background": "#fffbbb"});
+                                    $(that.options.selectors.georefLongitude).css({"background": "#fffbbb"});
+                                    $(that.options.selectors.georefDatum).css({"background": "#fffbbb"});                                
+                                    $(that.options.selectors.georefUncertainty).css({"background": "#fffbbb"});
+                                    $(that.options.selectors.georefUncertaintyUnits).css({"background": "#fffbbb"});
+                                    $(that.options.selectors.georefProtocol).css({"background": "#fffbbb"});
+                                    $(that.options.selectors.georefRemarks).css({"background": "#fffbbb"});	    
                                 });
-					        });
-					    }
+                            });
+                        }
                     )
                 }]
-			}
-		};
-	}
-		
-	var displayMap = function (source, georefs, callback) {
-	    var location = new google.maps.LatLng(georefs[0].decimalLatitude, georefs[0].decimalLongitude);
+            }
+        };
+    }
+    
+    var displayMap = function (source, georefs, callback) {
+        var location = new google.maps.LatLng(georefs[0].decimalLatitude, georefs[0].decimalLongitude);
         var bounds = new google.maps.LatLngBounds();
         var radius, contentString;
         var markers = [];
@@ -130,7 +130,7 @@ var cspace = cspace || {};
             });
         }
         
-      	var map = new google.maps.Map($('.map')[0], {zoom: 4, center: location, mapTypeId: google.maps.MapTypeId.ROADMAP});
+        var map = new google.maps.Map($('.map')[0], {zoom: 4, center: location, mapTypeId: google.maps.MapTypeId.ROADMAP});
         
         for (i=0; i<georefs.length; i++) {
             location = new google.maps.LatLng(georefs[i].decimalLatitude, georefs[i].decimalLongitude);
@@ -147,9 +147,9 @@ var cspace = cspace || {};
             });
             
             georefMarkerAssignment(marker, contentString);
-
+            
             georefBindSelectClick(i);
-                                                
+            
             var circle = new google.maps.Circle({
                 map: map,
                 radius: radius,
@@ -173,8 +173,8 @@ var cspace = cspace || {};
         
         map.fitBounds(bounds);
         
-	}
+    }
 	
     cspace.georeference.finalInit = function (that) {};
-		
+    
 })(jQuery, fluid_1_4);
